b4aac7465a650532d3e0ee48de169067f29b20e2
chan4899 <agrawalchandan4@gmail.com>
2021-04-17 15:14:21 +0530

    test scripts on voc_data for a video

--- scripts/test_voc_data.sh
+++ scripts/test_voc_data.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-python test.py --dataroot /home1/btp/pkb_btp_1/data/voc_data/ --name pascal_voc_01 --netG unet_128 --batch_size 64 --load_size 128 --crop_size 128 --preprocess resize_and_crop >> pascal_voc_01_out.txt
+python test.py --dataroot /home1/btp/pkb_btp_1/data/random_video/ --name pascal_voc_01 --netG unet_128 --batch_size 64 --load_size 128 --crop_size 128 --preprocess resize_and_crop >> test_random_video.txt
 
-python test.py --dataroot /home1/btp/pkb_btp_1/data/voc_data/ --name pascal_voc_02 --netG unet_128 --batch_size 64 --load_size 128 --crop_size 128 --preprocess resize_and_crop --use_sa_G --use_sa_D >> pascal_voc_02_out.txt
+python test.py --dataroot /home1/btp/pkb_btp_1/data/random_video/ --name pascal_voc_02 --netG unet_128 --batch_size 64 --load_size 128 --crop_size 128 --preprocess resize_and_crop --use_sa_G layer_3 --use_sa_D layer_3 >> test_random_video.txt
 
-python test.py --dataroot /home1/btp/pkb_btp_1/data/voc_data/ --name pascal_voc_03 --netG unet_128 --batch_size 64 --load_size 128 --crop_size 128 --preprocess resize_and_crop >> pascal_voc_03_out.txt
+python test.py --dataroot /home1/btp/pkb_btp_1/data/random_video/ --name pascal_voc_03 --netG unet_128 --batch_size 64 --load_size 128 --crop_size 128 --preprocess resize_and_crop >> test_random_video.txt
 
-python test.py --dataroot /home1/btp/pkb_btp_1/data/voc_data/ --name pascal_voc_04 --netG unet_128 --batch_size 64 --load_size 128 --crop_size 128 --preprocess resize_and_crop --use_sa_G --use_sa_D >> pascal_voc_04_out.txt
+python test.py --dataroot /home1/btp/pkb_btp_1/data/random_video/ --name pascal_voc_04 --netG unet_128 --batch_size 64 --load_size 128 --crop_size 128 --preprocess resize_and_crop --use_sa_G layer_3 --use_sa_D layer_3 >> test_random_video.txt
+
+python test.py --dataroot /home1/btp/pkb_btp_1/data/random_video/ --name pascal_voc_05 --netG unet_128 --batch_size 64 --load_size 128 --crop_size 128 --preprocess resize_and_crop --use_sa_G layer_2 --use_sa_D layer_2 >> test_random_video.txt
+
+python test.py --dataroot /home1/btp/pkb_btp_1/data/random_video/ --name pascal_voc_06 --netG unet_128 --batch_size 64 --load_size 128 --crop_size 128 --preprocess resize_and_crop --use_sa_G layer_2 --use_sa_D layer_2 >> test_random_video.txt
+
+python test.py --dataroot /home1/btp/pkb_btp_1/data/random_video/ --name pascal_voc_07 --netG unet_128 --batch_size 64 --load_size 128 --crop_size 128 --preprocess resize_and_crop --use_sa_G layer_1 --use_sa_D layer_1 >> test_random_video.txt
+
+python test.py --dataroot /home1/btp/pkb_btp_1/data/random_video/ --name pascal_voc_08 --netG unet_128 --batch_size 64 --load_size 128 --crop_size 128 --preprocess resize_and_crop --use_sa_G layer_1 --use_sa_D layer_1 >> test_random_video.txt
